a0e7d35ef1f6a11879bb475b2f7e8f83bdd75f2a
sojung park <sojungpark@sojungui-MacBook-Pro.local>
2016-06-09 15:07:44 +0900

    save triples as text file

--- src/OpenIE.java
+++ src/OpenIE.java
@@ -1,5 +1,8 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -52,8 +55,6 @@ public class OpenIE {
 	}
 	
 	public void extract(String path) {
-		HashSet<String> triples = new HashSet<String>();
-		
 		File dir = new File(path);		
 		
 		ArrayList<File> files = getFileList(dir);
@@ -61,19 +62,46 @@ public class OpenIE {
         for(File f : files) {
         	ArrayList<String> sentences = getSentencesFromFile(f);
         	
+        	HashSet<String> triples = new HashSet<String>();
+        	
         	HashMap<String, HashMap<String, String>> coreference = getCoreference(sentences);
         	
         	ArrayList<String> openIE_triples = compareOpenIE(sentences, coreference);
         	ArrayList<String> depend_triples = compareDepend(sentences, coreference);
         	
-        	//compare의 return 값 file save 하도록 변경.
         	triples.addAll(openIE_triples);
         	triples.addAll(depend_triples);
+        	
+        	System.out.println(triples.toString());
+        	
+        	String filename = f.toString();
+        	int lastSlash = filename.lastIndexOf("/");
+        	
+        	saveTriples(filename.substring(lastSlash+1), triples);
         }
-        
-        System.out.println(triples.toString());
 	}
 	
+	private void saveTriples(String filename, HashSet<String> triples) {
+		try
+        {
+            FileWriter fw = new FileWriter(filename+".txt"); // 절대주소 경로 가능
+            BufferedWriter bw = new BufferedWriter(fw);
+            
+            Iterator<String> iterator = triples.iterator(); 
+            while (iterator.hasNext()){           	
+            	bw.write(iterator.next()); 
+            	bw.newLine();
+            }
+            
+            bw.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println(e); 
+            System.exit(1);
+        }
+	}
+
 	public ArrayList<String> compareDepend(ArrayList<String> sentences, HashMap<String, HashMap<String, String>> coreference) {
 		// TODO Auto-generated method stub
 		HashMap<String, ArrayList<String>> triples = new HashMap<String, ArrayList<String>>();
@@ -105,7 +133,9 @@ public class OpenIE {
 			
 				for(int i=0; i<triple.size(); i++) {
 					String t = triple.get(i);
+					//System.out.println(t);
 					String[] tmp = t.split("\t");
+					//System.out.println("size: " + tmp.length);
 					//System.out.println(t);
 					if(coref.containsKey(tmp[1])) {
 						tmp[1] = coref.get(tmp[1]);
@@ -124,12 +154,7 @@ public class OpenIE {
 			}
 						
 		}
-		/*
-		System.out.println("->");
-		for(int i=0; i<result.size(); i++) {
-			System.out.println(i+1 + ": " + result.get(i));
-		}
-		*/
+
 		return result;
 	}
 
@@ -137,14 +162,7 @@ public class OpenIE {
 		
 		//str -> whole document...
 		//String doc = "Japanese share prices face headwinds next week amid jitters about a possible interest rate rise by the Bank of Japan later in the month, analysts said Friday. The Tokyo Stock Exchange's benchmark Nikkei-225 index lost 134.24 points or 0.78 percent to 17,091.59 in the week to January 5, during which it opened for only one and a half days and fell by 1.51 percent on Friday alone.";
-		
-		/*
-		//virtual doc
-		ArrayList<String> doc = new ArrayList<String>();
-		doc.add("Japanese share prices face headwinds next week amid jitters about a possible interest rate rise by the Bank of Japan later in the month, analysts said Friday.");
-		doc.add("The Tokyo Stock Exchange's benchmark Nikkei-225 index lost 134.24 points or 0.78 percent to 17,091.59 in the week to January 5, during which it opened for only one and a half days and fell by 1.51 percent on Friday alone.");
-		*/
-		
+				
     	//String doc =  StringUtils.join(sentences);
     	
 		//HashMap<String, HashMap<String, String>> coreference = getCoreference(doc);
@@ -442,15 +460,14 @@ public class OpenIE {
 			
 			//add triple to result array
 		}
-		
 		return result;
 	}
 	
 	public ArrayList<File> getFileList(File dir) { //dir 탐색해서 doc list 받고, doc 별로 extract triple 
 		//dir 탐색해서 파일 받아오기
 		
-		Date dt = new Date();
-		System.out.println(dt.toString() + "---------------");
+		//Date dt = new Date();
+		//System.out.println(dt.toString() + "---------------");
 		
 		//File dir = new File("D:/Downloads/LDC2015E45_TAC_KBP_Comprehensive_English_Source_Corpora_2009-2014/LDC2015E45_TAC_KBP_Comprehensive_English_Source_Corpora_2009-2014/data/1");
 		/*
@@ -470,7 +487,7 @@ public class OpenIE {
         }
         */
         
-        System.out.println(dt.toString() + "---------------");
+        //System.out.println(dt.toString() + "---------------");
         
         return files;
 	}
@@ -505,6 +522,7 @@ public class OpenIE {
 		return sentences;
 	}
 	
+	/*
 	public String getTriplesFromSentence(String str) { //sentence 별로 triple 뽑음
 		Annotation annotation = new Annotation(str);
 		pipeline.annotate(annotation);
@@ -519,18 +537,19 @@ public class OpenIE {
 
 			// Print the triples
 			for (RelationTriple triple : triples) {
-				/*
+				
 				System.out.println(triple.confidence + "\t" +
 				triple.subjectLemmaGloss() + "\t" +
 				triple.relationLemmaGloss() + "\t" +
 				triple.objectLemmaGloss());
-				*/
+				
 				result += triple.confidence + "\t" + "Gloss: " + triple.subjectGloss() + "/Head: " + triple.subjectHead() + "/" + triple.subjectLemmaGloss() + "\t" + triple.relationLemmaGloss() + "\t" + triple.objectLemmaGloss()+"\n";
 			}
 		}
 		//System.out.println(result);
 		return result;
 	}
+	*/
 	
 	private void visitAllFiles(ArrayList files, File dir) {		 
         if(dir.isDirectory()) {
